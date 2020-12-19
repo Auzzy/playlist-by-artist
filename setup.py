55ddb21108f764cbe0a5297c29eb207d1bd79495
Austin Noto-Moniz <metalnut4@netscape.net>
2020-12-18 22:42:55 -0500

    Setup as a consumable library.
    Add a setup.py. Wrap modules in a directory. Refactor scripts to be
    importable and reusable.
    
    One catch: there is no accounting for handling artist disambiguation
    apart from via the CLI.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='playlistmanager',
+    version='0.1',
+    author="Austin Noto-Moniz",
+    author_email="mathfreak65@gmail.com",
+    packages=find_packages(),
+    python_requires='>=3.6'
+)
