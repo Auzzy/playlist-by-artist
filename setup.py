7019616f8f1f75ba6dd5dbfe1e776e35f2571131
Austin Noto-Moniz <metalnut4@netscape.net>
2021-11-27 11:35:30 -0500

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,11 @@ setup(
     author="Austin Noto-Moniz",
     author_email="mathfreak65@gmail.com",
     packages=find_packages(),
-    python_requires='>=3.6'
+    python_requires=">=3.6",
+    install_requires=[
+        "requests >= 2.26.0",
+        "Unidecode >= 1.3.2",
+        "ytmusicapi >= 0.19.4, < 1.0",
+        "bs4"
+    ]
 )
